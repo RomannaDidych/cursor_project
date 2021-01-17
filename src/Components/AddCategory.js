a0f71f23ab43e8edb81a6b720af53cffbd271455
unknown <romanna.d@gmail.com>
2021-01-17 20:24:11 +0200

    added a category type selection

--- src/Components/AddCategory.js
+++ src/Components/AddCategory.js
@@ -17,16 +17,17 @@ class AddCategory extends Component{
 
 
 setInformation = async (e) =>{
-	e.preventDefault();
-	const inpN = e.target.elements.name.value;
-	const inpD = e.target.elements.description.value;
+	e.preventDefault();		
 	await this.setState({name: e.target.elements.name.value});
 	await this.setState({description: e.target.elements.description.value});
-	await this.setState({id: this.props.getID()})	
+	await this.setState({id: this.props.getID()})
+	await this.setState({type: e.target.elements.type.value})	
 	/*console.log(this.state.name);
 	console.log(this.state.description);
 	console.log(this.state.id);*/
-	this.props.addData(this.state);	
+	this.props.addData(this.state);
+	e.target.elements.name.value = '';
+	e.target.elements.description.value = '';	
 };
 
 addData = ()=> {};
@@ -36,14 +37,23 @@ render(){
 		<div>
 		<form className='addCategory' onSubmit={this.setInformation}>
 			<h2>New category</h2>
+			<div className='categoryType'>
+				<p>select type:</p>				
+				<select className='inpType' name="type" id="type">
+					<option value="income">income</option>
+    				<option value="charges">charges</option>
+				</select>				
+			</div>
+
 			<div className='categoryName'>
 				<p>name:</p>
 				<input className='inpName' type='text' name='name'/>				
 			</div>
 			<div className='categoryDescription' >
-				<p>Description:</p>
+				<p>description:</p>
 				<input className='inpDescr' type='text'  name='description'/>				
 			</div>
+
 			<button type='submit' className='btnAddNew' >add new category</button>
 		</form>
 		</div>
